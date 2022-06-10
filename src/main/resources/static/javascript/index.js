42f14eec383138ebf5bfc9ee9263d55a6d6d4b59
Matthew Guizar <matthewrguizar@gmail.com>
2022-06-10 12:33:54 -0700

    more tests

---
+++ src/main/resources/static/javascript/index.js
@@ -0,0 +1,13 @@
+const url = "https://onmylist.herokuapp.com/"
+
+const headers = {
+    "Content-Type" : "application/json"
+}
+
+const indexPage = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch (`${url}`, {
+
+    })
+}
