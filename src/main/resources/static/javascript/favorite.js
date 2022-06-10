87ac6509336736b21611813d375d5f0ebfcc2125
Matthew Guizar <matthewrguizar@gmail.com>
2022-06-10 13:08:06 -0700

    more tests

--- src/main/resources/static/javascript/favorite.js
+++ src/main/resources/static/javascript/favorite.js
@@ -49,10 +49,13 @@ const url = "https://onmylist.herokuapp.com/"
         <p class="card-text" data-product="${obj.id}"> ${obj.movie.title}</p>
         <p>${obj.movie.overview}</p>
         <div class="btn-group btn-group-toggle" data-toggle="buttons">
-  <label class="btn btn-secondary active">
-    <input type="radio" name="options" id="option1" autocomplete="off" checked> Active
-  </label>
-    </div>
+           <label class="btn btn-secondary active">
+             <input type="radio" name="options" id="option1" autocomplete="off" checked> Active
+            </label>
+              <label class="btn btn-secondary">
+                    <input type="radio" name="options" id="option2" autocomplete="off"> Radio
+               </label>
+        </div>
     </div>`
             favContainer.append(movieCard)
         })
