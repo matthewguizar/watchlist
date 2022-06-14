1ead3857aabc4f9a834aa8af387befff043a591c
Matthew Guizar <matthewrguizar@gmail.com>
2022-06-14 09:37:48 -0700

    fixing watched

--- src/main/resources/static/javascript/favorite.js
+++ src/main/resources/static/javascript/favorite.js
@@ -63,13 +63,13 @@ const deleteLibrary = async (libraryId) => {
        </div>
        <div class="form-check">
             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-            <label class="form-check-label" for="flexRadioDefault1">
+            <label class="form-check-label" for="flexRadioDefault1"  value="${!obj.watched}">
             True
             </label>
        </div>
        <div class="form-check">
             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-            <label class="form-check-label" for="flexRadioDefault2">
+            <label class="form-check-label" for="flexRadioDefault2"  value="${obj.watched}">
             False
             </label>
        </div>
