72fd5a176b252637f1b27b1e8e32721a08c41cf8
Matthew Guizar <matthewrguizar@gmail.com>
2022-06-14 08:58:07 -0700

    fixing watched

--- src/main/resources/static/javascript/favorite.js
+++ src/main/resources/static/javascript/favorite.js
@@ -64,9 +64,9 @@ const deleteLibrary = async (libraryId) => {
             <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Dropdown link</a>
             <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
+                <a class="dropdown-item">Action</a>
+                <a class="dropdown-item">Another action</a>
+                <a class="dropdown-item">Something else here</a>
         </div>
 </div>
     
