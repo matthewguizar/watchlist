3852c58783a71896518181f4366ddefd2c17ec0c
Matthew Guizar <matthewrguizar@gmail.com>
2022-06-16 07:13:16 -0700

    adding user registration username/password required

--- src/main/resources/static/javascript/register.js
+++ src/main/resources/static/javascript/register.js
@@ -25,23 +25,23 @@ const handleRegister = async (e) => {
         if (registerPass.value == ""){
             alert("Password is required")
             return false
-    }
+    } else {
+            const response = await fetch(`${url}users/register`, {
+                method: "POST",
+                body: JSON.stringify(bodyObj),
+                headers: headers
+            })
+                .catch(err => console.error(err.message));
 
-    const response = await fetch(`${url}users/register`, {
-        method: "POST",
-        body: JSON.stringify(bodyObj),
-        headers: headers
-    })
-        .catch(err => console.error(err.message));
+            const responseArr = await response.json();
 
-    const responseArr = await response.json();
+            if (response.status === 200) {
+                window.location.replace(responseArr[0])
+            }
+            if (response.status === 400) {
+                window.alert("Username already exists, please try again")
+            }
 
-    if (response.status === 200){
-        window.location.replace(responseArr[0])
-    }
-    if (response.status === 400){
-        window.alert("Username already exists, please try again")
-    }
-
+        }
 };
 registerForm.addEventListener("submit", handleRegister);
