e3fe8b6e54fd5ace6773dc27775df34672a62361
Matthew Guizar <matthewrguizar@gmail.com>
2022-06-16 06:59:12 -0700

    adding user registration username/password required

--- src/main/resources/static/javascript/register.js
+++ src/main/resources/static/javascript/register.js
@@ -14,6 +14,19 @@ const handleRegister = async (e) => {
         password: registerPass.value
     };
 
+        if (registerUsername.value == "" && registerPass.value == ""){
+            alert("Username and password required")
+            return false
+        }
+        if (registerUsername.value == "") {
+            alert("User name is required")
+            return false
+        }
+        if (registerPass.value == ""){
+            alert("Password is required")
+            return false
+    }
+
     const response = await fetch(`${url}users/register`, {
         method: "POST",
         body: JSON.stringify(bodyObj),
