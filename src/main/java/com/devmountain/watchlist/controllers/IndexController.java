4f2eaf72e5b85c938b89672cb9f1fe8e11aea69c
Matthew Guizar <matthewrguizar@gmail.com>
2022-06-16 06:32:52 -0700

    adding user login error page

--- src/main/java/com/devmountain/watchlist/controllers/IndexController.java
+++ src/main/java/com/devmountain/watchlist/controllers/IndexController.java
@@ -1,4 +1,23 @@
 package com.devmountain.watchlist.controllers;
 
+import com.devmountain.watchlist.dtos.UserDto;
+import com.devmountain.watchlist.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
 public class IndexController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("error")
+    public List<String> userLogin(@RequestBody UserDto userDto){
+        return userService.userLogin(userDto);
+    }
 }
